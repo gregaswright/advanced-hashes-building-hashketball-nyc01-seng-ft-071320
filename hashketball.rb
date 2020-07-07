3d2a5beaf7259942015245325c63630000d771b9
Gregory A.S. Wright <geslim@gmail.com>
2020-07-07 13:36:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,128 @@
 # Write your code here!
+
+def game_hash
+    {
+    home: {
+      team_name: "Brooklyn Nets"
+      colors: ["Black", "White"]
+      players:
+      {
+        player_name: "Alan Anderson"
+        number: 0
+        shoe: 16
+        points: 22
+        rebounds: 12
+        assists: 12
+        steals: 3
+        blocks: 1
+        slam_dunks: 1
+      },
+      {
+        player_name: "Reggie Evans"
+        number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      },
+      {
+        player_name: "Brook Lopez"
+        number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      },
+      {
+        player_name: "Mason Plumlee"
+        number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      },
+      {
+        player_name: "Jason Terry"
+        number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      }
+    }
+    away:
+      {
+      team_name: ""
+      colors: ["",""]
+      players:
+        {
+          player_name: ""
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        },
+        {
+          player_name: ""
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        },
+        {
+          player_name: ""
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        },
+        {
+          player_name: ""
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        },
+        {
+          player_name: ""
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        }
+      }
+    }
+  }
+end
